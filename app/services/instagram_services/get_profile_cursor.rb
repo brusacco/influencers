4a8c6848d2ee0cff29aa9057abddeb6d21dc328e
Bruno Sacco <brusacco@gmail.com>
2023-10-02 01:31:58 -0300

    fix issue

--- app/services/instagram_services/get_profile_cursor.rb
+++ app/services/instagram_services/get_profile_cursor.rb
@@ -11,12 +11,14 @@ module InstagramServices
 
     def call
       query_hash = 'eddbde960fed6bde675388aac39a3657'
-      par = Hash.new {}
+      par = {}
       par['id'] = @user_id
       par['first'] = '50'
       par['after'] = @cursor
 
-      url = "https://www.instagram.com/graphql/query/?query_hash=#{query_hash}&variables=#{par.to_json}"
+      url = CGI.escape("https://www.instagram.com/graphql/query/?query_hash=#{query_hash}&variables=#{par.to_json}")
+      url = "http://api.scrape.do?token=ed138ed418924138923ced2b81e04d53&url=#{url}"
+
       response = HTTParty.get(url)
       data = JSON.parse(response.body)
       handle_success(data)
