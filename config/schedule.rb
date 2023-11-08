6cc3ddade2225ecc8a225aa5ddd6e37b69fd4cbe
Bruno Sacco <brusacco@gmail.com>
2023-11-08 20:52:08 -0300

    asas

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,11 @@
 
 set :environment, 'production'
 
+every 3.hours do
+  rake 'instagram:crawler_top'
+  rake 'instagram:update_profiles_stats'
+end
+
 every 6.hours do
   rake 'instagram:test'
   rake 'instagram:update_profiles_stats'
