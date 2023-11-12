72a5b69fa40453410ad51e2ad1f9478d0ed018c9
Bruno Sacco <brusacco@gmail.com>
2023-11-12 16:34:46 -0300

    asas

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,7 +5,7 @@ class Profile < ApplicationRecord
   serialize :data, Hash
   has_many :instagram_posts, dependent: :destroy
 
-  enum :profile_type, [ :hombre, :mujer, :marca, :medio, :estatal ]
+  enum :profile_type, [ :hombre, :mujer, :marca, :medio, :estatal, :memes ]
 
   # As collaborator
   has_many :collaborated_collaborations,
