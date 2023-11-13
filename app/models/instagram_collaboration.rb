bf3da5d786f0c87ae8f6a43fa7157648ffaaf7f6
Bruno Sacco <brusacco@gmail.com>
2023-11-13 16:42:37 -0300

    asas

--- app/models/instagram_collaboration.rb
+++ app/models/instagram_collaboration.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class InstagramCollaboration < ApplicationRecord
-  belongs_to :instagram_post
+  belongs_to :instagram_post, touch: true
   belongs_to :collaborator, class_name: 'Profile'
   belongs_to :collaborated, class_name: 'Profile'
 
