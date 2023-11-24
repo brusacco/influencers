dba26b1d468f4a2faaabcd3026bd06f6aaf4c482
Bruno Sacco <brusacco@gmail.com>
2023-11-24 10:38:30 -0300

    aa

--- app/models/instagram_post.rb
+++ app/models/instagram_post.rb
@@ -48,9 +48,10 @@ class InstagramPost < ApplicationRecord
         next if STOP_WORDS.include?(cleaned_word)
         next if bads.include?(cleaned_word)
         next if cleaned_word.length <= 2
-        next if cleaned_word.start_with?('http', 'https', 'www')
+        next if cleaned_word.start_with?('http', 'https', 'www', 'whatsapp:')
         next if cleaned_word.match?(/\A\d+\z/) # Checks if the word is a number
         next if cleaned_word.match?(/\A\d+\W+\d+\z/) # Checks if the word is a number
+        next if cleaned_word.match?(/\(.*?\)/) # (xxxxxx)
 
         word_occurrences[cleaned_word] += 1
       end
