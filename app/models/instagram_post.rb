30be3d46ab13a39dd39a0e67a2e132e20ce7c4e1
Bruno Sacco <brusacco@gmail.com>
2023-11-24 11:45:18 -0300

    fix

--- app/models/instagram_post.rb
+++ app/models/instagram_post.rb
@@ -69,7 +69,8 @@ class InstagramPost < ApplicationRecord
     all.find_each do |post|
       next if post.caption.nil?
 
-      words = post.caption.gsub(/[[:punct:]]/, '').split
+      #words = post.caption.gsub(/[[:punct:]]/, '').split
+      words = post.caption.split
       bigrams = words.each_cons(2).map { |word1, word2| "#{word1.downcase} #{word2.downcase}" }
       bigrams.each do |bigram|
         next if bigram.split.first.length <= 2 || bigram.split.last.length <= 2
