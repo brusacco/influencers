c6405e76fa3a15ec8e892e576972f31971436b60
Bruno Sacco <brusacco@gmail.com>
2024-04-21 18:26:10 -0400

    Add section for posts with most likes in index.html.erb and update PostController#index to order posts by likes count

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,6 +4,7 @@ class PostController < ApplicationController
   def index
     @posts = InstagramPost.a_week_ago.order(total_count: :desc).limit(20)
     @videos = InstagramPost.a_week_ago.order(video_view_count: :desc).limit(20)
+    @likes = InstagramPost.a_week_ago.order(likes_count: :desc).limit(20)
     @comments = InstagramPost.a_week_ago.order(comments_count: :desc).limit(20)
   end
 end
