13c253c10bdaf3630b5325fdb51e01655fd4f9db
Bruno Sacco <brusacco@gmail.com>
2024-04-22 12:36:56 -0400

    Refactor schedule.rb to update Instagram rake tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,13 @@
 set :environment, 'production'
 
 every 6.hours do
-  rake 'instagram:test'
+  rake 'instagram:update_posts'
+end
+
+every 12.hours do
   rake 'instagram:update_profiles_stats'
+end
+
+every 24.hours do
+  rake 'instagram:update_profiles'
 end
