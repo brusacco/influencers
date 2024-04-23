3957acd02ada1d7e17fe949e2e098b0b899f0c10
Bruno Sacco <brusacco@gmail.com>
2024-04-23 12:13:56 -0400

    Refactor profiles_controller.rb to use 'paraguayos' scope for profile queries

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,9 +16,9 @@ class ProfilesController < ApplicationController
 
   def show
     if @profile.profile_type
-      @profiles = Profile.where(profile_type: @profile.profile_type).where.not(id: @profile.id).order(followers: :desc).limit(12)
+      @profiles = Profile.paraguayos.where(profile_type: @profile.profile_type).where.not(id: @profile.id).order(followers: :desc).limit(12)
     else
-      @profiles = Profile.where.not(id: @profile.id).order(total_interactions_count: :desc).limit(12)
+      @profiles = Profile.paraguayos.where.not(id: @profile.id).order(total_interactions_count: :desc).limit(12)
     end
 
     @mentions = Profile.where(username: @profile.mentions)
