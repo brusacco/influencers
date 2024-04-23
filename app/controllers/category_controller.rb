41243295cba88acc7c17a7d8847439801a155795
Bruno Sacco <brusacco@gmail.com>
2024-04-23 10:36:24 -0400

    Refactor category_controller.rb to use 'paraguayo' scope for profile queries

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,11 +3,11 @@
 class CategoryController < ApplicationController
   def show
     @category = params[:category_id]
-    @profiles = Profile.where(profile_type: @category).order(followers: :desc).limit(40)
-    @profiles_interactions = Profile.where(profile_type: @category).order(total_interactions_count: :desc).limit(20)
-    @profiles_video_views = Profile.where(profile_type: @category).order(total_video_view_count: :desc).limit(20)
-    @profiles_engagement = Profile.where(profile_type: @category).order(engagement_rate: :desc).limit(20)
-    @profiles_disaster = Profile.where(profile_type: @category).where(total_posts: 0).order(followers: :desc).limit(20)
+    @profiles = Profile.paraguayo.where(profile_type: @category).order(followers: :desc).limit(40)
+    @profiles_interactions = Profile.paraguayo.where(profile_type: @category).order(total_interactions_count: :desc).limit(20)
+    @profiles_video_views = Profile.paraguayo.where(profile_type: @category).order(total_video_view_count: :desc).limit(20)
+    @profiles_engagement = Profile.paraguayo.where(profile_type: @category).order(engagement_rate: :desc).limit(20)
+    @profiles_disaster = Profile.paraguayo.where(profile_type: @category).where(total_posts: 0).order(followers: :desc).limit(20)
 
     set_meta_tags title: "Top Influencers categoría #{@category.capitalize} | Influencers.com.py",
                   description: DESCRIPTION,
