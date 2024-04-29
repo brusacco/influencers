34efaf31484d8b0845a4ebeabd98e3dfdfe3006c
Bruno Sacco <brusacco@gmail.com>
2024-04-28 23:53:10 -0400

    Add rake tasks for updating collaborations and mentions profiles

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,14 +6,13 @@ every 1.hour do
   rake 'instagram:update_news_posts'
 end
 
-every 6.hours do
-  rake 'instagram:update_posts'
-end
-
 every 12.hours do
+  rake 'instagram:update_posts'
   rake 'instagram:update_profiles_stats'
 end
 
 every 24.hours do
   rake 'instagram:update_profiles'
+  rake 'util:collaborations'
+  rake 'util:add_mentions_profiles'
 end
