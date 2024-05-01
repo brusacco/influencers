9175266b21a356f55cd4c27d8fcb0ef656e89663
Bruno Sacco <brusacco@gmail.com>
2024-05-01 11:26:23 -0400

    Add country filter to profiles admin page

--- app/admin/profiles.rb
+++ app/admin/profiles.rb
@@ -49,6 +49,7 @@ ActiveAdmin.register Profile do
   #------------------------------------------------------------------
 
   filter :username
+  filter :country_string, as: :select, collection: %w[Paraguayos Otros]
   filter :profile_type, as: :select, collection: Profile.profile_types.map
   filter :category_name
   filter :is_private
