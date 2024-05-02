53ed0acf5af79791977e751bd714dc3e503c59a5
Bruno Sacco <brusacco@gmail.com>
2024-05-02 16:32:12 -0400

    Refactor Profile model scopes for better organization and readability

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,16 +2,16 @@
 
 set :environment, 'production'
 
-every 1.hour do
+every 6.hour do
   rake 'instagram:update_news_posts'
 end
 
 every 12.hours do
-  rake 'instagram:update_posts'
   rake 'instagram:update_profiles_stats'
 end
 
 every 24.hours do
+  rake 'instagram:update_posts'
   rake 'instagram:update_profiles'
   rake 'util:collaborations'
   rake 'util:add_mentions_profiles'
