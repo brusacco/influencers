d8aca394e8254ab1c501a4fc6d3a47af577d3f9b
Bruno Sacco <brusacco@gmail.com>
2024-07-05 11:42:53 -0400

    refactor: Remove conditional GETs in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,13 +22,6 @@ class ProfilesController < ApplicationController
       @profiles = Profile.paraguayos.where.not(id: @profile.id).order(total_interactions_count: :desc).limit(12)
     end
 
-    # Conditional GETs are a feature of the HTTP specification that
-    # provide a way for web servers
-    # to tell browsers that the response
-    # to a GET request hasn't changed since the last request
-    # and can be safely pulled from the browser cache.
-    fresh_when last_modified: @profiles.updated_at.utc, etag: @profiles
-
     @mentions = Profile.where(username: @profile.mentions)
 
     @posts = @profile.instagram_posts.order(posted_at: :desc).limit(12)
@@ -46,6 +39,13 @@ class ProfilesController < ApplicationController
     end
 
     @collabs = @profile.collaborated_collaborations.includes(:instagram_post).order(posted_at: :desc).limit(12)
+
+    # Conditional GETs are a feature of the HTTP specification that
+    # provide a way for web servers
+    # to tell browsers that the response
+    # to a GET request hasn't changed since the last request
+    # and can be safely pulled from the browser cache.
+    fresh_when last_modified: @profile.updated_at.utc, etag: @profile
 
     set_meta_tags title: "#{@profile.username} | Influencers.com.py",
                   description: DESCRIPTION,
