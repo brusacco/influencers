7bea04dc06f01bb2072343295d98044aa3e7b279
Bruno Sacco <brusacco@gmail.com>
2024-08-02 23:08:35 -0400

    Refactor InstagramServices get_posts_data.rb

--- app/services/instagram_services/get_posts_data.rb
+++ app/services/instagram_services/get_posts_data.rb
@@ -16,21 +16,10 @@ module InstagramServices
       response = HTTParty.get(api_url, format: :plain, timeout: 60)
       raise StandardError, 'Empty response body' if response.body.empty?
 
-      data1 = JSON.parse(response.body)
-      data1 = data1['data']['user']['edge_owner_to_timeline_media']['edges']
+      data = JSON.parse(response.body)
+      data = data['data']['user']['edge_owner_to_timeline_media']['edges']
 
-      if data['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
-        end_cursor = data['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
-        request = CGI.escape("https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables={\"id\":\"#{@profile.uid}\",\"first\":24,\"after\":\"#{end_cursor}\"}")
-        url = "http://api.scrape.do?token=ed138ed418924138923ced2b81e04d53&url=#{request}"
-        response = HTTParty.get(url, format: :plain, timeout: 60)
-        raise StandardError, 'Empty response body' if response.body.empty?
-
-        data2 = JSON.parse(response.body)
-        data2 = data2['data']['user']['edge_owner_to_timeline_media']['edges']
-      end
-
-      handle_success(data1 + data2)
+      handle_success(data)
     rescue StandardError => e
       handle_error(e.message)
     end
