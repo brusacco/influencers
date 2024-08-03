791d8f60da5b083b1352bd0e9319606cf8ae91e9
Bruno Sacco <brusacco@gmail.com>
2024-08-02 23:15:11 -0400

    aa

--- app/services/instagram_services/get_posts_data.rb
+++ app/services/instagram_services/get_posts_data.rb
@@ -6,22 +6,29 @@ module InstagramServices
       @profile = profile
     end
 
+    #------------------------------------------------------------------------------
+    #
+    #------------------------------------------------------------------------------
     def call
-      query = { id: @profile.uid, first: 24 }
+      data = api_call
+      data1 = data['data']['user']['edge_owner_to_timeline_media']['edges']
+      handle_success(data1)
+    rescue StandardError => e
+      handle_error(e.message)
+    end
+
+    #------------------------------------------------------------------------------
+    #
+    #------------------------------------------------------------------------------
+    def api_call(cursor: nil)
+      query = { id: @profile.uid, first: 24, cursor: }
       escaped_query = CGI.escape(query.to_json)
 
       request_url = "https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables=#{escaped_query}"
       api_url = "http://api.scrape.do?token=ed138ed418924138923ced2b81e04d53&url=#{CGI.escape(request_url)}"
 
       response = HTTParty.get(api_url, format: :plain, timeout: 60)
-      raise StandardError, 'Empty response body' if response.body.empty?
-
-      data = JSON.parse(response.body)
-      data = data['data']['user']['edge_owner_to_timeline_media']['edges']
-
-      handle_success(data)
-    rescue StandardError => e
-      handle_error(e.message)
+      JSON.parse(response.body)
     end
   end
 end
