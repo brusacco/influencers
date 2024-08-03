92c0e98d23ee5d5024f90d0c443331d706dfb58c
Bruno Sacco <brusacco@gmail.com>
2024-08-02 23:07:46 -0400

    aa

--- app/services/instagram_services/get_posts_data.rb
+++ app/services/instagram_services/get_posts_data.rb
@@ -7,16 +7,19 @@ module InstagramServices
     end
 
     def call
-      request = CGI.escape("https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables={\"id\":\"#{@profile.uid}\",\"first\":24}")
-      url = "http://api.scrape.do?token=ed138ed418924138923ced2b81e04d53&url=#{request}"
+      query = { id: @profile.uid, first: 24 }
+      escaped_query = CGI.escape(query.to_json)
 
-      response = HTTParty.get(url, format: :plain, timeout: 60)
+      request_url = "https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables=#{escaped_query}"
+      api_url = "http://api.scrape.do?token=ed138ed418924138923ced2b81e04d53&url=#{CGI.escape(request_url)}"
+
+      response = HTTParty.get(api_url, format: :plain, timeout: 60)
       raise StandardError, 'Empty response body' if response.body.empty?
 
       data1 = JSON.parse(response.body)
       data1 = data1['data']['user']['edge_owner_to_timeline_media']['edges']
 
-      if data['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page'] == true
+      if data['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
         end_cursor = data['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
         request = CGI.escape("https://www.instagram.com/graphql/query/?doc_id=17991233890457762&variables={\"id\":\"#{@profile.uid}\",\"first\":24,\"after\":\"#{end_cursor}\"}")
         url = "http://api.scrape.do?token=ed138ed418924138923ced2b81e04d53&url=#{request}"
