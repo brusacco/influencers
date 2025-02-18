5f84ce78f1917f05f43b6a833e4f323bb09e0779
Bruno Sacco <brusacco@gmail.com>
2025-02-18 17:20:22 -0300

    fix: Update profile data retrieval to use new field names and add nil handling

--- app/services/instagram_services/update_profile_data.rb
+++ app/services/instagram_services/update_profile_data.rb
@@ -11,20 +11,20 @@ module InstagramServices
 
       user = @data['data']['user']
       response = {
-        followers: user['edge_followed_by']['count'],
-        following: user['edge_follow']['count'],
+        followers: user['follower_count'],
+        following: user['following_count'],
         profile_pic_url: user['profile_pic_url'],
-        profile_pic_url_hd: user['profile_pic_url_hd'],
-        is_business_account: user['is_business_account'],
-        is_professional_account: user['is_professional_account'],
-        business_category_name: user['business_category_name'],
-        category_enum: user['category_enum'],
-        category_name: user['category_name'],
+        profile_pic_url_hd: user['hd_profile_pic_url_info']['url'] || nil,
+        is_business_account: user['is_business'] || nil,
+        is_professional_account: user['is_professional_account'] || nil,
+        business_category_name: user['business_category_name'] || nil,
+        category_enum: user['category_enum'] || nil,
+        category_name: user['category'],
         is_private: user['is_private'],
         is_verified: user['is_verified'],
         full_name: user['full_name'],
         biography: user['biography'],
-        is_joined_recently: user['is_joined_recently'],
+        is_joined_recently: user['is_joined_recently'] || nil,
         is_embeds_disabled: user['is_embeds_disabled'],
         uid: user['id']
       }
