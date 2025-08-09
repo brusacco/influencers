7998f412c5ef3280d9be5f00c12a909459b3aa49
Bruno Sacco <brusacco@gmail.com>
2025-08-09 09:14:05 -0300

    fix: Configure Puma with application directory and rackup file

--- config/puma.rb
+++ config/puma.rb
@@ -6,6 +6,10 @@ require 'puma/daemon'
 #
 environment ENV.fetch('RAILS_ENV', 'production')
 
+app_dir = '/home/rails/influencers'
+directory app_dir
+rackup "#{app_dir}/config.ru"
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
