deb53d481e2e1a50d3a85c4e249b9d79b6322457
Bruno Sacco <brusacco@gmail.com>
2025-08-14 18:24:01 -0300

    feat: Refactor TagsController show action and update show view for improved tag handling and display

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,10 +3,16 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find_by(id: params[:id])
-    @profiles = Profile.paraguayos.with_attached_avatar.tagged_with(@tag.name).order(followers: :desc).limit(40)
+
+    unless @tag
+      redirect_to root_path, alert: 'Tag not found'
+      return
+    end
+
+    profile_ids = Profile.paraguayos.with_attached_avatar.tagged_with(@tag.name).order(followers: :desc).limit(40).pluck(:id)
+    @profiles = Profile.where(id: profile_ids)
     @profiles_interactions = Profile.paraguayos.with_attached_avatar.tagged_with(@tag.name).order(total_interactions_count: :desc).limit(20)
-    @profiles_video_views = Profile.paraguayos.with_attached_avatar.tagged_with(@tag.name).order(total_video_view_count: :desc).limit(20)
-    @profiles_engagement = Profile.paraguayos.with_attached_avatar.tagged_with(@tag.name).order(engagement_rate: :desc).limit(20)
-    @profiles_disaster = Profile.paraguayos.with_attached_avatar.tagged_with(@tag.name).where(total_posts: 0).order(followers: :desc).limit(20)
+
+    @posts = InstagramPost.where(profile_id: profile_ids).a_week_ago
   end
 end
